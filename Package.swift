8ae39bf96746c49b3e1dcf2a9713d55279f0bea8
rbc33 <ricardobenthem@gmail.com>
2026-05-14 19:34:07 +0200

    change names in folder to Ghostbar

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "Ghostbar",
-            path: "Sources/Veil",
+            path: "Sources/Ghostbar",
             linkerSettings: [.linkedFramework("Carbon")]
         )
     ]
--- Sources/Veil/main.swift
+++
@@ -1,6 +0,0 @@
-import AppKit
-
-let app = NSApplication.shared
-let delegate = AppDelegate()
-app.delegate = delegate
-withExtendedLifetime(delegate) { app.run() }
